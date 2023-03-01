2de765a764c6dd08e0a3044c0f7032ce61374f49
TristanVarewijck <tristanconnect4@outlook.com>
2023-03-01 13:44:48 +0100

    fix: cms system

--- all-mobiel-cars/pages/index.tsx
+++ all-mobiel-cars/pages/index.tsx
@@ -8,11 +8,10 @@ import ServiceBlock from "../components/ServiceBlock";
 import { serviceBlockProps } from "../types";
 import { Check } from "react-feather";
 import Article from "../components/Article";
-import { articleProps } from "../types";
 import Link from "next/link";
 import Image from "next/image";
 
-const Dashboard = () => {
+const Dashboard = ({ articles, services }) => {
   const promotionCardsContent: promotionCardProps[] = [
     {
       uuid: "card1",
@@ -94,37 +93,6 @@ const Dashboard = () => {
       icon: <Check />,
       title: "Schoonmaak",
       href: "#",
-    },
-  ];
-
-  const aboutArticlesContent: articleProps[] = [
-    {
-      uuid: "article1",
-      title: "Al meer dan 15 jaar doen wij vertrouwdt onderhoud",
-      text: "Wij van All Mobiel Cars zijn een familiebedrijf dat is opgericht in het jaar 1990. Ons familiebedrijf werd opgericht in Wormerveer waar wij ook tegenwoordig nog steeds zijn terug te vinden. Het bedrijf is inmiddels wel overgegaan van vader op zoon, maar de uitstekende service en scherpe prijs waar wij in de loop der jaren bekendheid door hebben weten te verwerven zijn zoals vanouds nog steeds de sterke troeven van ons bedrijf.",
-      hasLink: true,
-      linkText: "Lees meer",
-      linkHref: "#",
-      image: "/images/articles/image-1.png",
-    },
-    {
-      uuid: "article2",
-      title: "All Mobiel Cars op RTL 7 en te zien op Videoland",
-      text: "All Mobiel Cars vindt het belangrijk om ten allen tijde een uitstekende klanttevredenheid te kunnen garanderen. In ons pand op het Zuideinde van Wormerveer verwelkomen we graag iedereen die op zoek is naar de best mogelijke service en prijs-kwaliteitverhouding voor zijn of haar auto. Het éénmalig verkopen van een auto is natuurlijk erg eenvoudig, maar het liefst beschikken wij over een tevreden en terugkerend cliënteel.",
-      hasLink: true,
-      linkText: "Lees meer",
-      linkHref: "#",
-      image: "/images/articles/image-2.png",
-    },
-    {
-      uuid: "article3",
-      title:
-        "Ocassions, ruilen, inkoop, verkoop, private lease en zakelijke lease",
-      text: "All Mobiel Cars is een merkonafhankelijk bedrijf. Dit zorgt er in de praktijk voor dat wij onze klanten steeds het meest waardevolle en objectieve advies kunnen geven. U heeft er namelijk niets aan om een dure auto te kopen die u helemaal niet nodig heeft. Wilt u graag een beroep kunnen doen op een eerlijk en transparant bedrijf welke merkonafhankelijk is? Ook in dat geval bent u bij ons beslist aan het juiste adres!",
-      hasLink: true,
-      linkText: "Lees meer",
-      linkHref: "#",
-      image: "/images/articles/image-3.png",
     },
   ];
 
@@ -228,16 +196,16 @@ const Dashboard = () => {
 
       <section className="about">
         <Heading titleText={"Over All Mobiel Cars"} align={"left"} />
-        {aboutArticlesContent.map((content, index) => {
+        {articles.data.map((content, index) => {
           return (
             <Article
-              uuid={content.uuid}
-              title={content.title}
-              text={content.text}
-              hasLink={content.hasLink}
-              linkHref={content.linkHref}
-              linkText={content.linkText}
-              image={content.image}
+              uuid={content.id}
+              title={content.attributes.title}
+              text={content.attributes.text}
+              hasLink={content.attributes.hasLink}
+              linkHref={content.attributes.linkHref}
+              linkText={content.attributes.linkText}
+              image={`http://localhost:1337${content.attributes.image.data.attributes.url}`}
               index={index}
             />
           );
@@ -246,5 +214,17 @@ const Dashboard = () => {
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  const articlesRes = fetch("http://localhost:1337/api/articles?populate=*");
+  const servicesRes = fetch("http://localhost:1337/api/services");
+  const responses = await Promise.all([articlesRes, servicesRes]);
+  return {
+    props: {
+      articles: await responses[0].json(),
+      services: await responses[1].json(),
+    },
+  };
+}
 
 export default Dashboard;
